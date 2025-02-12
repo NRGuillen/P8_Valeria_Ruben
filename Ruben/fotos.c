7d7fd91ed1cc2dd9de02ed9de1663404950feba6
Ruben <ruben.guillen.dosa@gmail.com>
2025-02-12 23:43:58 +0100

    Printf con colores y fix de la memoria dinamica para que añadan dragones

--- Ruben/fotos.c
+++ Ruben/fotos.c
@@ -1,18 +1,20 @@
-typedef struct dragones{
-
-    int id;
-    char nombre[20];
-    int vida;
-    int daño;
-    int resistencia;
-    int pasiva;
-    char descripcion[1000];
-    int oro;
-
-}dragones;
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_NOMBRE 50
+#define MAX_DESCRIPCION 1000
+
+typedef struct {
+    int id;
+    char nombre[MAX_NOMBRE];
+    int vida;
+    int dano;
+    int resistencia;
+    char pasiva[MAX_DESCRIPCION];
+    char descripcion[MAX_DESCRIPCION];
+    int oro;
+} Dragon;
 
 void añadir_dragon(char nombre, int vida, int daño, int resistencia, int pasiva, char descripcion, int oro){
 
@@ -31,7 +33,7 @@ void añadir_dragon(char nombre, int vida, int daño, int resistencia, int pasiv
     int seleccion;
 
     printf("Selecciona una pasiva para tu dragon:\n");
-    printf("1) PASIVA: +25%% resistencia a ataques\n")
+    printf("1) PASIVA: +25%% resistencia a ataques\n");
     printf("2) PASVIA: +25%% daño\n");
     printf("3) PASIVA: Se curara un 5%% de su vida despues de cada ataque\n");
     printf("Seleccion:");
@@ -46,115 +48,68 @@ void añadir_dragon(char nombre, int vida, int daño, int resistencia, int pasiv
     }
 
     printf("Introduce una descripcion para tu dragon:");
-    fgets(resistencia,1000,  stdin);
 
     printf("Introduce la recompensa de oro de tu dragon: ");
     scanf("%d", &oro);
 
 }
 
-void dragon1(){
-
-    printf("\n\n\t\t\tDragon 1\n");
-    system("chafa -f symbols -s 60x60 blood.jpg"); 
-    printf("\n\n<----CARACTERISTICAS---->\n");
-    printf("NOMBRE: Nigthmare.\n");
-    printf("VIDA: 100\n");
-    printf("DAÑO BASE: 10\n");
-    printf("RESISTENCIA BASE: 15%%");
-    printf("PASIVA: +25%% resistencia a ataques\n");
-    printf("DESCRIPCION: Nigthmare es un dragon proveniente de las pesadillas mas oscuras, nigun caballero quiere desafiarle\n");
-    printf("RECOMPENSA: 50 de oro\n");
-
+void inicializarDragon(Dragon *dragon, int id, const char *nombre, int vida, int dano, int resistencia, const char *pasiva, const char *descripcion, int oro) {
+    dragon->id = id;
+    strcpy(dragon->nombre, nombre);
+    dragon->vida = vida;
+    dragon->dano = dano;
+    dragon->resistencia = resistencia;
+    strcpy(dragon->pasiva, pasiva);
+    strcpy(dragon->descripcion, descripcion);
+    dragon->oro = oro;
 }
 
-void dragon2(){
-
-    printf("\n\n\t\t\tDragon 2\n");
-    system("chafa -f symbols -s 60x60 drake3.jpg");
-    printf("\n\n<----CARACTERISTICAS---->\n");
-    printf("NOMBRE: Cryonyx\n");
-    printf("VIDA: 100\n");
-    printf("DAÑO BASE: 15\n");
-    printf("RESISTENCIA BASE: 15\n");
-    printf("PASIVA: +25%% daño\n");
-    printf("DESCRIPCION: Cryonyx es un dragon temido debido a tu alto poder, vive en las montañas solitario y desarrollando aun mas su fuerza\n");
-    printf("RECOMPENSA: 100 de oro\n");
- 
+void mostrarDragon(const Dragon *dragon) {
+    printf("\n\t\033[31mDragon %d\033[0m\n", dragon->id);
+    printf("\033[31m\t Nombre:\033[0m %s\n", dragon->nombre);
+    printf("\033[31m\t Vida:\033[0m %d\n", dragon->vida);
+    printf("\033[31m\t Daño:\033[0m %d\n", dragon->dano);
+    printf("\033[31m\t Resistencia:\033[0m %d%%\n", dragon->resistencia);
+    printf("\033[31m\t Pasiva:\033[0m %s\n", dragon->pasiva);
+    printf("\033[31m\t Descripción:\033[0m %s\n", dragon->descripcion);
+    printf("\033[31m\t Recompensa:\033[0m %d de oro\n", dragon->oro);
 }
 
-void dragon3(){
+void visualizarDragones() {
+    Dragon dragones[3];
+    inicializarDragon(&dragones[0], 1, "Nightmare", 100, 10, 15, "+25% resistencia a ataques", "Nightmare es un dragón proveniente de las pesadillas más oscuras, ningún caballero quiere desafiarle.", 50);
+    inicializarDragon(&dragones[1], 2, "Cryonyx", 100, 15, 15, "+25% daño", "Cryonyx es un dragón temido debido a su alto poder, vive en las montañas en solitario desarrollando aún más su fuerza.", 100);
+    inicializarDragon(&dragones[2], 3, "Velkhanos", 100, 10, 15, "Se curará un 5% de su vida después de cada ataque", "Velkhanos es un dragón histórico, proveniente de Asia, ningún otro dragón ha podido derrotarle.", 200);
 
-    printf("\n\n\t\t\tDragon 3\n");
-    system("chafa -f symbols -s 60x60 drake4.jpg");
-    printf("\n<----CARACTERISTICAS---->\n");
-    printf("NOMBRE: Velkhanos\n");
-    printf("VIDA: 100\n");
-    printf("DAÑO BASE: 10\n");
-    printf("RESISTENCIA BASE: 15%%\n");
-    printf("PASIVA: Se curara un 5%% de su vida despues de cada ataque\n");
-    printf("DESCRIPCION: Velkhanos es un dragon historico, proveniente de asia, ningun otro dragon a podido derrotarle\n");
-    printf("RECOMPENSA: 200 de oro\n");
-
-}
-
-
-void visualizar_dragones(){
-
-    dragon1();
-    dragon2();
-    dragon3();
-
-    int seleccion;
-    printf("\n\nIntroduce contra que dragon te quieres enfrentar primero:");
-    scanf("%d", &seleccion);
-    printf("\n\n");
-
-    if(seleccion == 1){
-        printf("<-----------------Has seleccionado el dragon %d------------->", seleccion);
-        dragon1();
-    }else if(seleccion == 2){
-        dragon2();
-    }else if(seleccion == 3){
-        dragon3();
-    }else{
-        printf("No existe ningun dragon con el numero %d", seleccion);
-    }
-
-
+    printf("\033[35mDRAGONES DISPONIBLES:\033[0m\n");
+    system("chafa -f symbols -s 50x30 blood.jpg"); //PARA USAR CHAFA, EL USUARIO HA DE DESCARGARSE "sudo apt install chafa"
+    mostrarDragon(&dragones[0]);
+    system("chafa -f symbols -s 50x30 drake3.jpg");
+    mostrarDragon(&dragones[1]);
+    system("chafa -f symbols -s 50x30 drake4.jpg");
+    mostrarDragon(&dragones[2]);
 
 }
 
 int main() {
+    int opcion;
+    printf("1) Seleccionar un dragón predefinido\n");
+    printf("2) Salir\n");
+    printf("Seleccion: ");
+    scanf("%d", &opcion);
 
-    int opciones;
-
-    printf("1) Seleccionar un dragon predefinido\n");
-    printf("2) Crear un dragon personalizado\n");
-    printf("3) Salir\n");
-    printf("Seleccion:");
-    scanf("%d", &opciones);
-
-    switch(opciones){
-
+    switch(opcion) {
         case 1:
-
-            visualizar_dragones();
-       
+            visualizarDragones();
             break;
-
         case 2:
-
+            printf("Saliendo...\n");
             break;
-
-        case 3:
-            break;
-
         default:
+            printf("Opción inválida.\n");
             break;
-
     }
 
-    
     return 0;
 }
