2ca16206a638ace861886693cdb07d51a6378b89
Ruben <ruben.guillen.dosa@gmail.com>
2025-02-19 19:12:08 +0100

    No se porque no funciona el getchar de resistencia

--- Ruben/dragones.c
+++ Ruben/dragones.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
+#include "utilidades.h"
 
 #define MAX_NOMBRE 50
 #define MAX_DESCRIPCION 1000
@@ -30,87 +30,113 @@ void añadir_dragon(Dragon **dragones, int *totalDragones) {
     Dragon nuevoDragon;
     int seleccion;
 
-    getchar(); 
+    printf(AMARILLO"\nIntroduzca los datos del nuevo dragon: \n"SC);
 
-    printf("Introduce el nombre de tu dragón: ");
+    printf(AZUL_C"\tID: %d\n", 1 + MAX_DRAGONES);
+
+    getchar();  
+
+    // NOMBRE
+    printf(AZUL_C"\tNOMBRE: ");
     scanf("%[^\n]", nuevoDragon.nombre);
 
-    printf("Introduce la vida de tu dragón: ");
-    if (scanf("%d", &nuevoDragon.vida) != 1) {
-        printf("Entrada no válida. Por favor, introduce solo números.\n");
-    }
-    if(nuevoDragon.vida > 100 || nuevoDragon.vida < 0){
-        printf("El rango de vida tiene que estar entre 0-100\n");
-    }
+    // VIDA
+    do {
+        printf(AZUL_C"\tVIDA: ");
+        if (scanf("%d", &nuevoDragon.vida) != 1) {
+            printf(ROJO"\tPor favor, introduce solo números.\n\n"SC);
+            while (getchar() != '\n');  
+        } else if (nuevoDragon.vida < 0 || nuevoDragon.vida > 100) {
+            printf(ROJO"\tEl rango de vida tiene que estar entre 0-100\n\n"SC);
+        }
+    } while (nuevoDragon.vida < 0 || nuevoDragon.vida > 100);
 
     getchar(); 
 
-    printf("Introduce el daño de tu dragón: ");
-    if (scanf("%d", &nuevoDragon.daño) != 1) {
-        printf("Entrada no válida. Por favor, introduce solo números.\n");
-    }    if(nuevoDragon.daño > 50 || nuevoDragon.daño < 0){
-        printf("El rango de daño tiene que estar entre 0-50\n");
-    }
+    // DAÑO
+    do {
+        printf(AZUL_C"\tDAÑO: ");
+        if (scanf("%d", &nuevoDragon.daño) != 1) {
+            printf(ROJO"\tPor favor, introduce solo números.\n\n"SC);
+            while (getchar() != '\n');  
+        } else if (nuevoDragon.daño < 0 || nuevoDragon.daño > 50) {
+            printf(ROJO"\tEl rango de daño tiene que estar entre 0-50\n\n"SC);
+        }
+    } while (nuevoDragon.daño < 0 || nuevoDragon.daño > 50);
 
     getchar(); 
 
-    printf("Introduce la resistencia de tu dragón: ");
-    if (scanf("%d", &nuevoDragon.resistencia) != 1) {
-        printf("Entrada no válida. Por favor, introduce solo números.\n");
-    }    if(nuevoDragon.daño > 20 || nuevoDragon.daño < 0){
-        printf("El rango de daño tiene que estar entre 0-20\n");
-    }
+    // RESISTENCIA
+    do {
+        printf(AZUL_C"\tRESISTENCIA: ");
+        if (scanf("%d", &nuevoDragon.resistencia) != 1) {
+            printf(ROJO"\tPor favor, introduce solo números.\n\n"SC);
+            while (getchar() != '\n');  
+        } else if (nuevoDragon.daño < 0 || nuevoDragon.resistencia > 20) {
+            printf(ROJO"\tEl rango de resistencia tiene que estar entre 0-20\n\n"SC);
+        }
+    } while (nuevoDragon.resistencia < 0 || nuevoDragon.resistencia > 20);
 
-    printf("Selecciona una pasiva para tu dragón:\n");
-    printf("\t1) +25%% resistencia a ataques\n");
-    printf("\t2) +25%% daño\n");
-    printf("\t3) Se curará un 5%% de su vida después de cada ataque\n");
-    printf("\t4) Sin pasiva\n");
-    printf("Selección: ");
+    getchar(); 
+
+
+    // PASIVA
+    printf(AZUL_C"\tPASIVA:\n");
+    printf(ROJO"\t  1) +25%% resistencia a ataques\n");
+    printf(ROJO"\t  2) +25%% daño\n");
+    printf(ROJO"\t  3) Se curará un 5%% de su vida después de cada ataque\n");
+    printf(ROJO"\t  4) Sin pasiva\n");
+    printf(MAGENTA"\tSelección: "SC);
     scanf("%d", &seleccion);
     getchar(); 
 
     switch (seleccion) {
         case 1:
-            strcpy(nuevoDragon.pasiva, "+25% resistencia a ataques");
+            strcpy(nuevoDragon.pasiva, SC"+25% resistencia a ataques");
             break;
         case 2:
-            strcpy(nuevoDragon.pasiva, "+25% daño");
+            strcpy(nuevoDragon.pasiva, SC"+25% daño");
             break;
         case 3:
-            strcpy(nuevoDragon.pasiva, "Se curará un 5% de su vida después de cada ataque");
+            strcpy(nuevoDragon.pasiva, SC"Se curará un 5% de su vida después de cada ataque");
             break;
         default:
-            strcpy(nuevoDragon.pasiva, "Sin pasiva");
+            strcpy(nuevoDragon.pasiva, SC"Sin pasiva");
             break;
     }
 
-    printf("Introduce una descripción para tu dragón: ");
+    // DESCRIPCIÓN
+    printf(AZUL_C"\tDESCRIPCIÓN: ");
     scanf("%[^\n]", nuevoDragon.descripcion);
-
     getchar(); 
 
-    printf("Introduce la recompensa de oro de tu dragón: ");
-    if (scanf("%d", &nuevoDragon.oro) != 1) {
-        printf("Entrada no válida. Por favor, introduce solo números.\n");
-    }    if(nuevoDragon.oro > 200 || nuevoDragon.oro < 0){
-        printf("El rango de daño tiene que estar entre 0-200\n");
-    }
+    // RECOMPENSA 
+    do {
+        printf(AZUL_C"\tRECOMPENSA: ");
+        if (scanf("%d", &nuevoDragon.oro) != 1) {
+            printf(ROJO"\tPor favor, introduce solo números.\n\n"SC);
+            while (getchar() != '\n');  
+        } else if (nuevoDragon.oro < 0 || nuevoDragon.oro > 200) {
+            printf(ROJO"\tEl rango de recompensa tiene que estar entre 0-200\n\n"SC);
+        }
+    } while (nuevoDragon.oro < 0 || nuevoDragon.oro > 200);
 
     nuevoDragon.id = (*totalDragones) + 1;
 
     *dragones = (Dragon *)realloc(*dragones, (*totalDragones + 1) * sizeof(Dragon));
     if (*dragones == NULL) {
-        printf("Error al asignar memoria\n");
+        printf(ROJO"\tError al asignar memoria\n\n");
         return;
     }
 
-     memcpy(&((*dragones)[*totalDragones]), &nuevoDragon, sizeof(Dragon));
+    memcpy(&((*dragones)[*totalDragones]), &nuevoDragon, sizeof(Dragon));
 
-    (*totalDragones)++; 
-    printf("\nDragón personalizado:\n");
+    (*totalDragones)++;
+
+    printf(NARANJA"\n\tDragón agregado:\n");
     mostrarDragon(&nuevoDragon);
 }
+
 
 void inicializarDragon(Dragon *dragon, int id,  char *nombre, int vida, int daño, int resistencia,  char *pasiva,  char *descripcion, int oro) {
     dragon->id = id;
@@ -124,14 +150,14 @@ void inicializarDragon(Dragon *dragon, int id,  char *nombre, int vida, int dañ
 }
 
 void mostrarDragon(Dragon *dragon) {
-    printf("\033[36mDragon %d\033[0m\n", dragon->id);
-    printf("\033[31mNombre:\033[0m %s\n", dragon->nombre);
-    printf("\033[31mVida:\033[0m %d\n", dragon->vida);
-    printf("\033[31mDaño:\033[0m %d\n", dragon->daño);
-    printf("\033[31mResistencia:\033[0m %d%%\n", dragon->resistencia);
-    printf("\033[31mPasiva:\033[0m %s\n", dragon->pasiva);
-    printf("\033[31mDescripción:\033[0m %s\n", dragon->descripcion);
-    printf("\033[31mRecompensa:\033[0m %d de oro\033[30B\n\n", dragon->oro);
+    printf(ROJO"\tID:"SC" %d\n", dragon->id);
+    printf(ROJO"\tNombre:"SC" %s\n", dragon->nombre);
+    printf(ROJO"\tVida:"SC" %d\n", dragon->vida);
+    printf(ROJO"\tDaño:"SC" %d\n", dragon->daño);
+    printf(ROJO"\tResistencia:"SC" %d%%\n", dragon->resistencia);
+    printf(ROJO"\tPasiva:"SC" %s\n", dragon->pasiva);
+    printf(ROJO"\tDescripción:"SC" %s\n", dragon->descripcion);
+    printf(ROJO"\tRecompensa:"SC" %d de oro\n\n", dragon->oro);
 }
 
 void visualizarDragones() {
@@ -145,7 +171,7 @@ void visualizarDragones() {
     inicializarDragon(&dragones[1], 2, "Cryonyx", 100, 15, 15, "+25% daño", "Cryonyx es un dragón temido debido a su alto poder, vive en las montañas en solitario desarrollando aún más su fuerza.", 100);
     inicializarDragon(&dragones[2], 3, "Velkhanos", 100, 10, 15, "Se curará un 5% de su vida después de cada ataque", "Velkhanos es un dragón histórico, proveniente de Asia, ningún otro dragón ha podido derrotarle.", 200);
 
-    printf("\033[35mDRAGONES DISPONIBLES:\033[0m\n");
+    printf(NARANJA"\nDRAGONES DISPONIBLES:\n"SC);
     system("chafa -f symbols -s 50x30 Nightmare.jpg"); 
     mostrarDragon(&dragones[0]);
     system("chafa -f symbols -s 50x30 Cryonyx.jpg");
@@ -160,22 +186,38 @@ void visualizarDragones() {
 
 void seleccionarDragon(Dragon *dragones, int totalDragones) {
     int seleccion;
-    printf("Introduce el número del dragón que quieres seleccionar (1-3): ");
-    scanf("%d", &seleccion);
+    int valido = 0;
 
-    if (seleccion >= 1 && seleccion <= totalDragones) {
-        if (seleccion == 1) {
-            system("chafa -f symbols -s 50x30 Nightmare.jpg");  
-        } else if (seleccion == 2) {
-            system("chafa -f symbols -s 50x30 Cryonyx.jpg");  
-        } else if (seleccion == 3) {
-            system("chafa -f symbols -s 50x30 Velkhanos.jpg");  
+    do {
+        printf(NARANJA"Introduzca el ID del dragon con el que va a jugar: ");
+        scanf("%d", &seleccion);
+
+        if (seleccion >= 1 && seleccion <= totalDragones) {
+            switch(seleccion) {
+                case 1:
+                    printf("\n\nDragon seleccionado: %d\n", seleccion);
+                    printf("\n");
+                    system("chafa -f symbols -s 50x30 Nightmare.jpg");
+                    break;
+                case 2:
+                    printf("\n\nDragon seleccionado: %d\n", seleccion);
+                    printf("\n");
+                    system("chafa -f symbols -s 50x30 Cryonyx.jpg");
+                    break;
+                case 3:
+                    printf("\n\nDragon seleccionado: %d\n", seleccion);
+                    printf("\n");
+                    system("chafa -f symbols -s 50x30 Velkhanos.jpg");
+                    break;
+            }
+
+            mostrarDragon(&dragones[seleccion - 1]);
+
+            valido = 1;
+        } else {
+            printf(ROJO"Has introducido un dragón que no existe.\n\n"SC);
         }
-
-        mostrarDragon(&dragones[seleccion - 1]);  
-    } else {
-        printf("Has introducido un dragón que no existe.\n");
-    }
+    } while (!valido);
 }
 
 
@@ -184,10 +226,11 @@ int main() {
     Dragon *dragones = NULL; 
     int totalDragones = 0;  
 
-    printf("1) Seleccionar un dragón predefinido\n");
-    printf("2) Crear un nuevo dragón\n");
-    printf("3) Salir\n");
-    printf("Seleccion: ");
+    printf(MAGENTA"\tMENÚ:\n");
+    printf(AZUL_C"\t1) Seleccionar un dragón predefinido\n");
+    printf(AZUL_C"\t2) Crear un nuevo dragón\n");
+    printf(AZUL_C"\t3) Salir\n");
+    printf(MAGENTA"\tSeleccion: ");
     scanf("%d", &opcion);
 
     switch(opcion) {
